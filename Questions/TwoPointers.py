9b318d20748c1c2c04292760403034e2f642e04e
saket <saketbhatnagar2@gmail.com>
2025-07-03 18:42:30 +0530

    twoPointers

--- Questions/TwoPointers.py
+++ Questions/TwoPointers.py
@@ -1,10 +1,12 @@
 class Solution:
-    def __init__():
+    def __init__(self):
         pass
-    def MoveZeroes(self,arr:list)->list:
-	    left = 0
-	    for right in range(len(arr)):
-	        if arr[right]:
-	            arr[right],arr[left] = arr[left],arr[right] 
-	            left+=1
-	    return arr
+
+    def MoveZeroes(self, arr: list) -> list:
+        left = 0
+        for right in range(len(arr)):
+            if arr[right] != 0:
+                arr[right], arr[left] = arr[left], arr[right]
+                left += 1
+        return arr
+    
